710b5edef7952abeeb24cbec3c23980351ff2717
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-07 12:34:58 -0800

    Add survey componenet with info, no form yet

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Survey() {
+function Survey(props){
   return (
-    <h1>Survey</h1>
+    <React.Fragment>
+      <div onClick = {() => props.whenSurveyClicked(props.id)}>
+        <h3>{props.name}</h3>
+        <h6>{props.description}</h6>
+        <p><em>{props.question1}</em></p>
+        <p><em>{props.question2}</em></p>
+        <p><em>{props.question3}</em></p>
+        <p><em>{props.question4}</em></p>
+      </div>
+    </React.Fragment>
   );
 }
 
-export default Survey;
+Survey.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  question1: PropTypes.string,
+  question2: PropTypes.string,
+  question3: PropTypes.string,
+  question4: PropTypes.string,
+  id: PropTypes.string,
+  whenSurveyClicked: PropTypes.func
+};
+
+export default Survey
