97045d7674f0d8a80268f0fdcc6e21a72bfa5d44
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-07 14:57:28 -0800

    Add and configure bindings and update entry point file

--- src/index.js
+++ src/index.js
@@ -5,12 +5,26 @@ import App from './components/App';
 import { createStore } from 'redux';
 import { Provider  } from 'react-redux';
 import rootReducer from './reducers/index';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
 
 const store = createStore(rootReducer);
 
+const rrfProps = {
+  firebase,
+  config: {
+        userProfile: "users"
+    },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
 );
