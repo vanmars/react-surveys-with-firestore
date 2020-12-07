61112e47ec791c4bfb907df5319d6c56475b1748
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-07 14:58:48 -0800

    Add firestore reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,15 @@ import surveyListReducer from './survey-list-reducer';
 import formVisibleReducer from './form-visible-reducer';
 import selectedSurveyReducer from './selected-survey-reducer';
 import editingReducer from './editing-reducer';
+import { firestoreReducer } from 'redux-firestore';
+
 
 const rootReducer = combineReducers({
   masterSurveyList: surveyListReducer,
   formVisible: formVisibleReducer,
   selectedSurvey: selectedSurveyReducer,
-  editing: editingReducer
+  editing: editingReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
