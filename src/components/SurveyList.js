c8840463329829de3afa2122a95416f789acc495
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-07 12:31:16 -0800

    Add SurveyList

--- src/components/SurveyList.js
+++ src/components/SurveyList.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import Survey from  './Survey';
 
-function SurveyList() {
+
+function SurveyList(props) {
   return (
-    <h1>Survey List</h1>
+    <React.Fragment>
+     <hr/>
+     {props.surveyList.map((survey) => 
+      <Survey
+        whenSurveyClicked = { props.onSurveySelection }
+        name={survey.name}
+        description={survey.descrption}
+        question1={survey.question1}
+        question2={survey.question2}
+        question3={survey.question3}
+        question4={survey.question4}
+        id={survey.id}
+        key={survey.id}/>
+     )}
+    </React.Fragment>
   );
 }
+
+SurveyList.propTypes = {
+  surveyList: PropTypes.array,
+  onSurveySelection: PropTypes.func
+};
 
 export default SurveyList;
