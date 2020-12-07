10079d4b1f6cf40c8aee648a8932b3357b40d0bc
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-07 12:28:21 -0800

    Add EditSurveyForm and update NewSurveyForm form submission handler

--- src/components/EditSurveyForm.js
+++ src/components/EditSurveyForm.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
 
-function EditSurveyForm() {
+function EditSurveyForm (props) {
+  const { survey, onEditSurvey } = props;
+
+  function handleEditSurveyFormSubmission(event) {
+    event.preventDefault();
+    onEditSurvey({name: event.target.name.value, description: event.target.description.value, question1: event.target.question1.value, question2: event.target.question2.value, question3: event.target.question3.value, question4: event.target.question4.value, survey: survey.id});
+  }
+  
   return (
-    <h1>Edit Survey Form</h1>
+    <React.Fragment>
+      <ReusableForm 
+        formSubmissionHandler={handleEditSurveyFormSubmission}
+        buttonText="Update Survey" />
+    </React.Fragment>
   );
 }
+
+EditSurveyForm.propTypes = {
+  survey: PropTypes.object,
+  onEditSurvey: PropTypes.func
+};
 
 export default EditSurveyForm;
--- src/components/NewSurveyForm.js
+++ src/components/NewSurveyForm.js
@@ -8,7 +8,7 @@ function NewSurveyForm(props){
 
   function handleNewSurveyFormSubmission(event) {
     event.preventDefault();
-    props.onNewSurveyCreation({name: event.target.name.value, description: event.target.description.value, id: v4()});
+    props.onNewSurveyCreation({name: event.target.name.value, description: event.target.description.value, question1: event.target.question1.value, question2: event.target.question2.value, question3: event.target.question3.value, question4: event.target.question4.value, id: v4()});
     }
   
   return (
