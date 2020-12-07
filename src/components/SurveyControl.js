5dd2a2d0538ef475fb8c7b29c12a80cc85129697
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-07 11:18:52 -0800

    Add SurveyControl

---
+++ src/components/SurveyControl.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SurveyControl extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render () {
+    return(
+      <h2>Surveys</h2>  
+    );
+  }
+}
+
+export default SurveyControl;
