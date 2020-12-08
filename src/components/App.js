e177985db7e2e344bb647c834812b294cf0ee824
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-08 09:42:15 -0800

    Add routing in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Header from './Header';
 import SurveyControl from './SurveyControl';
+import Sigin from './Signin';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <React.Fragment>
+    <Router>
       <Header />
-      <SurveyControl />
-    </React.Fragment>
+      <Switch>
+        <Route path='/sigin'>
+          <Sigin />
+        </Route>
+        <Route path='/'>
+          <SurveyControl />
+        </Route>
+        <SurveyControl />
+      </Switch>
+    </Router>
   );
 }
 
